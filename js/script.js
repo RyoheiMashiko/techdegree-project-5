bb8eb8d702590e1ab65f7acc0db0cc8687e28847
RyoheiMashiko <RyoheiMashiko@users.noreply.github.com>
2020-07-07 17:22:29 +0900

    Updated.

--- js/script.js
+++ js/script.js
@@ -4,4 +4,18 @@ lightbox.option({
   disableScrolling: true
 })
 
-$('#search').
+document.getElementById("search").addEventListener("keyup", inputValue);
+
+function inputValue() {
+  let keyWord = document.getElementById("search").value.toUpperCase();
+  console.log(keyWord);
+}
+  let caption = document.getElementsByTagName("a")[0].getAttribute("data-title");
+  console.log(caption);
+
+
+
+
+  // console.log(inputText);
+// const myHeading = document.querySelector("h1");
+// myHeading.textContent = inputText;
