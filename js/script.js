2005dc321568dd5cf049e5ec4c992eddd7b054ae
RyoheiMashiko <RyoheiMashiko@users.noreply.github.com>
2020-07-07 22:13:26 +0900

    Updated.

--- js/script.js
+++ js/script.js
@@ -4,16 +4,21 @@ lightbox.option({
   disableScrolling: true
 })
 
-document.getElementById("search").addEventListener("keyup", inputValue);
+document.getElementById("search").addEventListener("keydown", inputValue);
 
 function inputValue() {
   let keyWord = document.getElementById("search").value.toUpperCase();
   
   let i;
   for(i = 0; i < 12; i++) {
-    let caption = document.getElementsByTagName("a")[i].getAttribute("data-title").toUpperCase(); 
-    let n = caption.includes(keyWord);
-    console.log(n);
+      let caption = document.getElementsByTagName("a")[i].getAttribute("data-title").toUpperCase(); 
+      let n = caption.includes(keyWord);
+      console.log(n);
+      if( n !== true ) {
+        document.getElementsByTagName("a")[i].style.display = 'none';
+      } else {
+        document.getElementsByTagName("a")[i].style.visibility = 'visible';
+      }
     }
   }
 
