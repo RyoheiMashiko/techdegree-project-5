24145d6d8ba5348cf82d88e9ca72ce6a94f83835
RyoheiMashiko <RyoheiMashiko@users.noreply.github.com>
2020-07-07 18:49:23 +0900

    Updated.

--- js/script.js
+++ js/script.js
@@ -8,19 +8,12 @@ document.getElementById("search").addEventListener("keyup", inputValue);
 
 function inputValue() {
   let keyWord = document.getElementById("search").value.toUpperCase();
-  console.log(keyWord);
-}
+  
   let i;
-  for(i = 0; i < 12; i++){
-  let caption = document.getElementsByTagName("a")[i].getAttribute("data-title");
-  console.log(caption);
-}
-  // let caption = document.getElementsByTagName("a")[1].getAttribute("data-title");
-  // console.log(caption);
+  for(i = 0; i < 12; i++) {
+    let caption = document.getElementsByTagName("a")[i].getAttribute("data-title").toUpperCase(); 
+    let n = caption.includes(keyWord);
+    console.log(n);
+    }
+  }
 
-
-
-
-  // console.log(inputText);
-// const myHeading = document.querySelector("h1");
-// myHeading.textContent = inputText;
